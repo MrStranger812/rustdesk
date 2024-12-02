0ff4c95a73d2671d918a82504509a6e71c0924b8
MrStranger812 <163648488+MrStranger812@users.noreply.github.com>
2024-12-02 10:53:18 +0330

    Update main.rs
    changing the rustdesk to hamrahdesk

--- src/main.rs
+++ src/main.rs
@@ -50,10 +50,10 @@ fn main() {
         -k, --key=[KEY] ''
        -s, --server=[] 'Start server'",
     );
-    let matches = App::new("rustdesk")
+    let matches = App::new("hamrahdesk")
         .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .author("Hamrah Hesabe Amin Ltd<info[at]hamrahhesabeamin[dot]com>")
+        .about("HamrahDesk command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
